68e073b6e37ee24deb9b159ae55257d40d3af77d
Gururaj F V <gururaj.fv@craftsilicon.onmicrosoft.com>
2025-06-18 11:18:43 +0530

    version updated to 16

--- src/app/data/components/upload-popup/upload-popup.module.ts
+++ src/app/data/components/upload-popup/upload-popup.module.ts
@@ -3,9 +3,9 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
 import { MatLegacyDialogModule as MatDialogModule } from "@angular/material/legacy-dialog";
 import { MaterialModule } from "src/app/modules/material/material.module";
 import { ButtonModule } from "../button/button.module";
-import { NgxFileDropModule } from "ngx-file-drop";
 import { UploadPopupComponent } from "./upload-popup.component";
 import { TextInputModule } from "../text-input/text-input.module";
+import { NgxFileDropModule } from "ngx-file-drop";
 
 @NgModule({
     declarations: [UploadPopupComponent],
